9b9259b241aef0e34ff44b8f6545556f3bcf3b63
Samuel Garcia <burntwaffles99@gmail.com>
2023-10-29 00:40:36 -0700

    committed

--- Number Guessing Game.py
+++ Number Guessing Game.py
@@ -1,11 +1,36 @@
 import random
+word_dict = {0: "1st", 1: "2nd", 2: "3rd"}
 
-mode = input("Want to choose a number or guess?\n Type (c) to be the chooser or (g) to be the guesser:\n ")
-possible_numbers = range(0,9999)
+print("Both you and the CPU have 3 tries to guess the other's number.")
+while True:
+    mode = input("Want to choose a number or guess?\nType (c) to choose or (g) to guess:\n")
+    if mode in "cg" and len(mode) == 1:
+        break
 
-if mode == "c":
-    choice = input("Choose a number between 0 and 9999:\n")
-if mode == "g":
-    cpu_choice = random.choice(possible_numbers)
-
-
+def choose_mode():
+    while True:
+        usr_choice = input("Choose a number between 0 and 10:\n")
+        if usr_choice.isdigit() is False:
+            continue
+        if 10 >= int(usr_choice) >= 0:
+            break
+    counter = 0
+    min_num = 0
+    max_num = 10
+    while counter <= 3:
+        if counter == 3:
+            print("-----You Win-----")
+            break
+        cpu_guess = random.randint(min_num, max_num)
+        print(f"{word_dict[counter]} Number: {cpu_guess}")
+        usr_reply = input("Is my number HIGHER (h), LOWER (l), or CORRECT (c):\n")
+        if usr_reply == "h":
+            counter += 1
+            max_num = cpu_guess - 1
+        elif usr_reply == "l":
+            counter += 1
+            min_num = cpu_guess + 1
+        elif usr_reply == "c":
+            print(f"CPU's GUESS: {cpu_guess}\nYOUR NUMBER: {usr_choice}\nCOMPARISON: {(int(cpu_guess) == int(usr_choice))}")
+            break
+choose_mode()
