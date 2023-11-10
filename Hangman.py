7b20c70296504bb26f2ccec397488aa0ba20cfc9
Samuel Garcia <burntwaffles99@gmail.com>
2023-11-10 12:36:25 -0800

    committed

--- Hangman.py
+++ Hangman.py
@@ -1,4 +1,23 @@
 import random
 
-fruits = ["Apple", "Banana", "Pineapple", "Kiwi", "Orange", "Tomato", "Grape", "Cherry", "Strawberry", "Lime", "Pear", "Melon", "Peach"]
-vegetables = ["Leek", "Beet", "Caper", "Carrot", "Celery", "Garlic", "Lentil", "Fennel", "Cabbage", "Broccoli", "Onion", "Artichoke"]
+MAX_TRIES = 5
+
+WORD_LISTS = {
+    "Fruits" : ["Apple", "Banana", "Pineapple", "Kiwi", "Orange", "Tomato", "Grape", "Cherry", "Strawberry", "Lime", "Pear", "Melon", "Peach"],
+    "Vegetables" : ["Leek", "Beet", "Caper", "Carrot", "Celery", "Garlic", "Lentil", "Fennel", "Cabbage", "Broccoli", "Onion", "Artichoke"],
+    "Animals" : ["Dog", "Lion", "Lizard", "Cat", "Bear", "Peacock", "Guinea Pig", "Turtle", "Frog", "Fish", "Elephant", "Raccoon", "Zebra"],
+    "Companies" : ["Apple", "Tesla", "Mcdonalds", "Walmart", "Toyota", "Sony", "Microsoft", "Disney", "Nestle", "Intel", "Amazon", "Costco"],
+    "Foods" : ["Crepe", "Pizza", "Churro", "Sushi", "Taco", "Burger", "Hot Dog", "Peking Duck", "Pie", "Curry", "Chilaquiles", "Pho", "Bolognese"]
+}
+
+topics = list(WORD_LISTS.keys())
+chosen_topic = random.choice(topics)
+chosen_word = random.choice(WORD_LISTS[chosen_topic])
+let_len = len(chosen_word)
+
+print(f"-----HANGMAN-----\nTOPIC: {chosen_topic}\n")
+counter = 0
+while True:
+    letter_chosen = input(f"({counter+1}/{MAX_TRIES}) CHOOSE A LETTER:\n")
+    if letter_chosen.isdigit() == True:
+        continue
